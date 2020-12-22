efc7ed838c749aab5f49a21b3b8c9fc8f2a1c471
GNVageesh <vageeshgn2005@gmail.com>
2020-12-22 19:29:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height:600,
+        minHeight: 500,
+        minWidth: 500,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+        }
+    });
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
